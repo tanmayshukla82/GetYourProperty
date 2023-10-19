a3358e076fbb8a20706e97f1f03ba5793f631db3
Tanmay Shukla <tanmayshukla82@gmail.com>
2023-10-19 16:52:55 +0530

    database setting and user model creation

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("Server at port 3000.");
